51b1b1b503e2ebb0a6b4228ff5ab8d0682d51eea
miguebaci <45130632+miguebaci@users.noreply.github.com>
2019-10-08 19:30:50 -0300

    MovieController created

---
+++ Controllers/MovieController.php
@@ -0,0 +1,22 @@
+<?php
+    namespace Controllers;
+
+    use DAO\IMovieDAO as IMovieDAO;
+    use DAO\MovieDAO as MovieDAO;
+    use Models\Movie as Movie;
+
+    class MovieController
+    {
+        private $movieDAO;
+
+        public function __construct()
+        {
+            $this->movieDAO = new movieDAO();
+        }
+        public function ShowListView()
+        {
+            $movieList = $this->movieDAO->GetAll();
+
+            require_once(VIEWS_PATH."movie-list.php");
+        }
+?>
