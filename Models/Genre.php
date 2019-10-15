c0445e62667cf91dce5e7afd7b80fe47d6c7e327
Carlos Ducamp <ducampcarlos@gmail.com>
2019-10-15 02:37:16 -0300

    Added ID to Genre Model

--- Models/Genre.php
+++ Models/Genre.php
@@ -5,10 +5,20 @@
 
     class Genre
     {   
+        private $idGenre;
         private $description;
 
-        public function __construct($description){
+        public function __construct($idGenre,$description){
+            $this->idGenre=$idGenre;
             $this->description = $description;
+        }
+
+        public function setIdGenre($idGenre){
+            $this->idGenre=$idGenre;
+        }
+
+        public function getIdGenre(){
+            return $this->idGenre;
         }
 
         public function getDescription()
