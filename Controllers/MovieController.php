2afbe47f80db509396775a18b9f2be996e0195b1
Carlos Ducamp <ducampcarlos@gmail.com>
2019-10-15 18:36:17 -0300

    Changed the Movie Posters size

--- Controllers/MovieController.php
+++ Controllers/MovieController.php
@@ -2,12 +2,12 @@
     namespace Controllers;
 
     use DAO\IMovieDAO as IMovieDAO;
-    use DAO\MovieDAO as MovieDAO;
-    //use DAO\MovieDAOJSON as MovieDAO;
+    //use DAO\MovieDAO as MovieDAO;
+    use DAO\MovieDAOJSON as MovieDAO;
 
     use DAO\IGenreDAO as IGenreDAO;
-    use DAO\GenreDAO as GenreDAO;
-    //use DAO\GenreDAOJSON as GenreDAO;
+    //use DAO\GenreDAO as GenreDAO;
+    use DAO\GenreDAOJSON as GenreDAO;
 
     use Models\Movie as Movie;
     use Models\Genre as Genre;
--- Views/movie-list.php
+++ Views/movie-list.php
@@ -23,7 +23,7 @@
                                              <td><?php echo $movie->getLanguage() ?></td>
                                              <td><?php echo $movie->getDuration() ?></td>
                                              <td><?php foreach(is_string($movie->getIdGenre()) ? json_decode($movie->getIdGenre() , true) : $movie->getIdGenre() as $genres) {echo $genreRepo->GetById($genres)->getDescription()."/"; } ?></td>
-                                             <td><?php echo "<".POSTER_ROOT . $movie->getImage().">" ?></td>
+                                             <td><?php echo "<".POSTER_ROOT . $movie->getImage()." width='180' height='240'>" ?></td>
                                         </tr>
                                    <?php
                               }
