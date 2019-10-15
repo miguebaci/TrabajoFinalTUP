8185e5bf2e3b6453d724867ee8124e0d4b826bf1
Carlos Ducamp <ducampcarlos@gmail.com>
2019-10-15 04:10:40 -0300

    The Movie List now shows the Genre names

--- Controllers/MovieController.php
+++ Controllers/MovieController.php
@@ -4,7 +4,11 @@
     use DAO\IMovieDAO as IMovieDAO;
     //use DAO\MovieDAO as MovieDAO;
     use DAO\MovieDAOJSON as MovieDAO;
+    use DAO\IGenreDAO as IGenreDAO;
+    //use DAO\GenreDAO as GenreDAO;
+    use DAO\GenreDAOJSON as GenreDAO;
     use Models\Movie as Movie;
+    use Models\Genre as Genre;
 
     class MovieController
     {
@@ -17,7 +21,7 @@
         public function ShowListView()
         {
             $movieList = $this->movieDAO->GetAll();
-
+            $genreRepo = new GenreDAO();
             require_once(VIEWS_PATH."movie-list.php");
         }
         public function UpdateMovies(){
