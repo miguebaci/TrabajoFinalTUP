1aa3dfb5780b5b75cf553d5c9cc03e35187f1c65
Carlos Ducamp <ducampcarlos@gmail.com>
2019-10-15 07:07:38 -0300

    Fixes to interactions with json

--- Controllers/MovieController.php
+++ Controllers/MovieController.php
@@ -4,9 +4,11 @@
     use DAO\IMovieDAO as IMovieDAO;
     use DAO\MovieDAO as MovieDAO;
     //use DAO\MovieDAOJSON as MovieDAO;
+
     use DAO\IGenreDAO as IGenreDAO;
     use DAO\GenreDAO as GenreDAO;
     //use DAO\GenreDAOJSON as GenreDAO;
+
     use Models\Movie as Movie;
     use Models\Genre as Genre;
 
