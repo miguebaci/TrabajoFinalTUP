df3799a11582b139bbef5a0cb5887e863e4203ec
Miguel Angel Bacigaluppi <miguebaci@gmail.com>
2019-10-18 17:37:49 -0300

    added schema for the database

--- Schema/Schema.sql
+++ Schema/Schema.sql
@@ -29,15 +29,43 @@ CREATE TABLE movieXgenre
 (
 	idMovie INT NOT NULL,
     idGenre INT NOT NULL,
-    constraint pfk_idMovie FOREIGN KEY(idMovie) references movie(idMovie),
-    constraint pfk_idGenre FOREIGN KEY(idGenre) references genre(idGenre)
+    constraint pfk_idMovie FOREIGN KEY (idMovie) references movie(idMovie),
+    constraint pfk_idGenre FOREIGN KEY (idGenre) references genre(idGenre)
 )Engine=InnoDB;
 
-CREATE TABLE movieXgenre
+CREATE TABLE moviefunction
+(   idMovieFunction INT AUTO_INCREMENT NOT NULL,
+    idMovie INT NOT NULL,
+    idCinema INT NOT NULL,
+    function_date DATE,
+    function_time TIME,
+    constraint pk_idMovieFunction PRIMARY KEY (idMovieFunction),
+    constraint fk_idMovie FOREIGN KEY (idMovie) references movie(idMovie),
+    constraint fk_idGenre FOREIGN KEY (idCinema) references cinema(idMovie)
+)Engine=InnoDB;
+
+CREATE TABLE user
 (
-	idMovie INT NOT NULL,
-    idGenre INT NOT NULL,
-    PRIMARY KEY (idMovie, idGenre),
-    constraint pfk_idMovie FOREIGN KEY(idMovie) references movie(idMovie),
-    constraint pfk_idGenre FOREIGN KEY(idGenre) references genre(idGenre)
+    idUser INT AUTO_INCREMENT NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    idUserProfile INT NOT NULL,
+    idRole INT NOT NULL,
+    constraint pk_idUser PRIMARY KEY (idUser),
+    constraint fk_idUserProfile FOREIGN KEY (idUserProfile) references userProfile(idUserProfile),
+    constraint fk_idRole FOREIGN KEY (idRole) references userRole(idRole)
+)Engine=InnoDB;
+
+CREATE TABLE userProfile
+(
+    idUserProfile INT AUTO_INCREMENT NOT NULL,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    dni INT NOT NULL,
+)Engine=InnoDB;
+
+CREATE TABLE userRole
+(
+    idRole INT NOT NULL PRIMARY KEY,
+    role_description VARCHAR(100) NOT NULL
 )Engine=InnoDB;
