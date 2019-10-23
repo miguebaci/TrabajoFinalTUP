ffbcddca144535d1aefd7271cd6ff8c6557ffeaa
Carlos Ducamp <ducampcarlos@gmail.com>
2019-10-23 20:50:08 -0300

    Finished Register and added Login

---
+++ Views/login.php
@@ -0,0 +1,32 @@
+<?php
+    require_once('nav.php');
+?>
+<main class="py-5">
+     <section id="listado" class="mb-5">
+          <div class="container">
+               <h2 class="mb-4">Login</h2>
+               <form action="<?php echo FRONT_ROOT ?>User/LoginValidation" method="post" class="bg-light-alpha p-5">
+                    <div class="row">                         
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="">Email</label>
+                                   <input type="email" name="email" value="" class="form-control" required>
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="">Contraseña</label>
+                                   <input type="password" name="password" value="" class="form-control" required>
+                              </div>
+                         </div>
+                         </div>
+                         <div>
+                              <div class="form-group">
+                              <button type="submit" name="button" class="btn btn-dark ml-auto d-block">Login</button>
+                              </div>
+                         </div>
+                    </div>
+               </form>
+          </div>
+     </section>
+</main>
---
+++ Views/register.php
@@ -0,0 +1,39 @@
+<?php
+    require_once('nav.php');
+?>
+<main class="py-5">
+     <section id="listado" class="mb-5">
+          <div class="container">
+               <h2 class="mb-4">Registro</h2>
+               <form action="<?php echo FRONT_ROOT ?>User/RegisterValidation" method="post" class="bg-light-alpha p-5">
+                    <div class="row">                         
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <input type="hidden" name="role" value="User">
+                                   <label for="">Email</label>
+                                   <input type="email" name="email" value="" class="form-control" required>
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="">Contraseña</label>
+                                   <input type="password" name="password" value="" class="form-control" required>
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="">Confirmar contraseña</label>
+                                   <input type="password" name="password2" value="" class="form-control" required>
+                              </div>
+                         </div>
+                         </div>
+                         <div>
+                              <div class="form-group">
+                              <button type="submit" name="button" class="btn btn-dark ml-auto d-block">Registro</button>
+                              </div>
+                         </div>
+                    </div>
+               </form>
+          </div>
+     </section>
+</main>
