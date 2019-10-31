dfb4a7a78fa8d3d0cf37dcbf7781b0cc6f8b1c3b
Carlos Ducamp <ducampcarlos@gmail.com>
2019-10-31 18:43:31 -0300

    Minor Fix

--- Views/nav.php
+++ Views/nav.php
@@ -15,19 +15,7 @@
                </li>  
                <li class="nav-item">
                     <a class="nav-link" href="<?php echo FRONT_ROOT ?>Movie/ShowListView">Listar peliculas</a>
-               </li>
-               <li class="nav-item dropdown">
-               <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Admin Actions
-               </a>
-               <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="<?php echo FRONT_ROOT ?>Cinema/ShowAddView">Add Cinema</a>
-                    <a class="dropdown-item" href="<?php echo FRONT_ROOT ?>Cinema/ShowListView">List Cinemas</a>
-                    <a class="dropdown-item" href="<?php echo FRONT_ROOT ?>Movie/UpdateMovies">Update Movies</a>
-                    <a class="dropdown-item" href="<?php echo FRONT_ROOT ?>Movie/ShowListView">List Movies</a>
-                    <a class="dropdown-item" href="<?php echo FRONT_ROOT ?>Genre/UpdateGenres">Update Genres</a>
-               </div>
-               </li>  
+               </li> 
                <li class="nav-item dropdown">
                     <select name="AdminOptions"  onchange="location = this.value;" class="nav-link dropdown-toggle" >
                          <option class="dropdown-item" value="">Admin Options</option> 
