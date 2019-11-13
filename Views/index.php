fc2feeddb1407c2ff9299d4b84d880f500c6418a
miguebaci <miguebaci@gmail.com>
2019-11-12 21:57:01 -0300

    method add to dao, made the skeleton of the index

--- Views/index.php
+++ Views/index.php
@@ -1,7 +1,92 @@
 <?php
     require_once('nav.php');
+    require_once('get-functions.php');
 ?>
-<main class="py-5">
+
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<body>
+<!-- Container for the image gallery -->
+<div class="container">
+<?php
+foreach ($movieList as $movie) {
+     
+?>
+  <!-- Full-width images with number text -->
+  <div class="mySlides">
+    <div class="numbertext"></div>
+      <img src="<?php $movie->getImage()?>" style="width='180' height='240'">
+  </div>
+  <?php
+  }
+  ?>
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+  <!-- Image text -->
+  <div class="caption-container">
+    <p id="caption"></p>
+  </div>
+
+  <!-- Thumbnail images -->
+  <div class="row">
+    <div class="column">
+      <img class="demo cursor" src="img_woods.jpg" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="img_5terre.jpg" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="img_mountains.jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="img_lights.jpg" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="img_nature.jpg" style="width:100%" onclick="currentSlide(5)" alt="Nature and sunrise">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="img_snow.jpg" style="width:100%" onclick="currentSlide(6)" alt="Snowy Mountains">
+    </div>
+  </div>
+</div>
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+</script>
+
+</body>
+</html>
+
+<!--<main class="py-5">
      <section id="listado" class="mb-5">
           <div class="container">
                <h1>Index</h1>
@@ -10,8 +95,7 @@
                     <input type="number" name="discount" placeholder="Discount" id="" required>
                     <input type="number" name="quantity" placeholder="Quantity" id="" required>
                     <input type="submit" value="submit">
-                    <!-- QR <img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=Peliculas&choe=UTF-8" title="Link to Google.com" />  -->
                </form>
           </div>
      </section>
-</main>
+</main>-->
