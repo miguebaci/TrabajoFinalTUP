6abe898dec49baec4bef29b90a819432e83544aa
Carlos Ducamp <ducampcarlos@gmail.com>
2019-11-16 04:58:48 -0300

    Added info when the users does a buy, and the total paid in the purchases in the user profile

--- Views/show-user-purchases.php
+++ Views/show-user-purchases.php
@@ -13,6 +13,7 @@
                          <th>Function Date</th>
                          <th>Ticket Number</th>
                          <th>Ticket Quantity</th>
+                         <th>Total</th>
                          <th>QR</th>
                     </thead>
                     <tbody>
@@ -30,6 +31,7 @@
                                              <td><?php echo $function->getDate()." ".$function->getTime() ?></td>
                                              <td><?php echo $ticket->getTicketNumber() ?></td>
                                              <td><?php echo $purchase->getTicketQuantity() ?></td>
+                                             <td><?php echo "$".($purchase->getTotal() - ($purchase->getTotal()*$purchase->getDiscount()/100)) ?></td>
                                              <td><?php echo "<img src=".$ticket->getQR()."title='Your Ticket' />" ?></td>
                                         </tr>
                                    <?php
