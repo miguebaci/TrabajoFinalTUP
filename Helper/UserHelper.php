8f1c96d9d71c58f42dd755dbfc5af1c8229c11ee
Carlos Ducamp <ducampcarlos@gmail.com>
2019-11-19 01:32:07 -0300

    Fixed Helper

--- Helper/UserHelper.php
+++ Helper/UserHelper.php
@@ -25,24 +25,16 @@
     class UserHelper{
 
         private $cinemaDAO;
-        private $roomDAO;
         private $movieDAO;
-        private $genreDAO;
         private $functionDAO;
         private $purchaseDAO;
 
         public function __construct()
         {   
             $this->cinemaDAO = new CinemaDAO();
-            $this->roomDAO = new RoomDAO();
             $this->movieDAO = new MovieDAO();
-            $this->genreDAO = new GenreDAO();
             $this->functionDAO = new FunctionDAO();
             $this->purchaseDAO = new PurchaseDAO();
-        }
-
-        function helpMovieList(){
-            return $this->movieDAO->GetAll();
         }
 
         function helpGetCinemas(){
@@ -51,16 +43,6 @@
 
         function helpGetCinemaById($idCinema){
             return $this->cinemaDAO->GetById($idCinema);
-        }
-
-        function helpGetCinemasByMovie(Movie $movie){
-            return $this->cinemaDAO->GetByMovie($movie);
-        }
-
-        function helpGetRoomsByCinema(Cinema $cinema){
-            
-            return $this->roomDAO->GetAllByCinemaId($cinema->getIdCinema());
-        
         }
 
         function helpPurchasesByCinema($cinema, $dateStart, $dateEnd){
@@ -72,46 +54,33 @@
         }
 
         function helpAllMoviesInFunctions(){
-            
             return $this->functionDAO->GetAllMoviesInFunctions();
-        
+        }
+
+        function helpBringUserPurchases($user){
+            return $this->purchaseDAO->bringUserPurchases($user);
         }
      
         function helpMovieById($idMovie){
             return $movie=$this->movieDAO->GetById($idMovie);
         }
 
-        function helpGetRoom($idRoom){
-            return $this->roomDAO->GetById($idRoom);
-        }
-
-        function helpGetGenre($idGenre){
-            return $this->genreDAO->GetById($idGenre);
-        }
-
         function getCinemaDAO(){
             return $this->cinemaDAO;
-        }
-        
-        function getRoomDAO(){
-            return  $this->roomDAO;
         }
 
         function getMovieDAO(){
             return $this->movieDAO;
         }
 
-        function getGenreDAO(){
-            return $this->genreDAO;
-        }
-
         function getPurchaseDAO(){
             return $this->purchaseDAO;
         }
 
-        function getUserDAO(){
-            return $this->userDAO;
+        function getFunctionDAO(){
+            return $this->functionDAO;
         }
+
     }
 
 ?>
