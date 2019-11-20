640c13927d486817c6972872b2f0a4c591fd64b4
Carlos Ducamp <ducampcarlos@gmail.com>
2019-11-20 16:42:56 -0300

    Minor Analytics fix

--- Views/show-movie-analytics.php
+++ Views/show-movie-analytics.php
@@ -5,7 +5,7 @@
      <section id="listado" class="mb-5">
           <div class="container">
                <h3>Sales for the movie <?php echo $movie->getMovieName(); echo $_POST["date_start"]==$_POST["date_end"] ? "<br>For the date of ".$_POST["date_start"] : "<br>For the dates between ".$_POST["date_start"]." and ".$_POST["date_end"]; ?> </h3>
-               <h5>Total recollected: <?php echo $analytics["Total"]; ?></h5>
+               <h5>Total recollected: $<?php echo $analytics["Total"]; ?></h5>
                <h5>Tickets sold: <?php echo $analytics["Tickets"]; ?></h5>
           </div>
      </section>
