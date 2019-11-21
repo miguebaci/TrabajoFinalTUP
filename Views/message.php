e6328b8f0c1e0a1a63e512474e2ff572ef9f526d
Carlos Ducamp <ducampcarlos@gmail.com>
2019-11-21 15:00:39 -0300

    Defined User Messages

--- Views/message.php
+++ Views/message.php
@@ -4,9 +4,18 @@
 
 <main class="py-5">
     <section id="listado" class="mb-5">
-        <div class="container alert alert-dark">
-                <?php echo $message; ?>
-                <a class="btn btn-dark" href=<?php echo $location; ?>>Continue</a> 
+        <div class="container">
+            <div class="row h-100">
+                <div class="col-sm-12 my-auto">
+                    <div class="card card-block w-25 mx-auto"><h3 align="center">
+                        <?php echo $message; ?></h3>
+                        <a class="btn btn-dark d-block" href=<?php echo $location; ?>>Continue</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </main>
+
+
+
