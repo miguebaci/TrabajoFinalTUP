879723f87e7b62e409b70afda3574ae0f0275efb
miguebaci <miguebaci@gmail.com>
2020-02-05 16:00:42 -0300

    optimized login/register visuals

--- Views/login.php
+++ Views/login.php
@@ -3,36 +3,25 @@
     require_once(VIEWS_PATH."fbload.php");
 ?>
 
-<main class="py-5">
+<main class="d-flex align-items-center justify-content-center height-100 py-5">
      <section id="listado" class="mb-5">
           <div class="container">
-               <h2 class="mb-4">Login</h2>
-               <form action="<?php echo FRONT_ROOT ?>User/LoginValidation" method="post" class="bg-light-alpha p-5">
-                    <div class="row">                         
-                         <div class="col-lg-4">
-                              <div class="form-group">
-                                   <label for="">Email</label>
-                                   <input type="email" name="email" value="" class="form-control" required>
-                              </div>
-                         </div>
-                         <div class="col-lg-4">
-                              <div class="form-group">
-                                   <label for="">Contraseña</label>
-                                   <input type="password" name="password" value="" class="form-control" required>
-                              </div>
-                         </div>
-                         </div>
-                         <div>
-                              <div class="form-group">
-                                   <button type="submit" name="button" class="btn btn-dark ml-auto d-block">Log in</button>
-                                   <?php echo '<a class="btn btn-primary ml-auto float-right" href="' . htmlspecialchars($loginUrl) . '">Or log in with Facebook!</a>'; ?>
-                              </form>
-                              </div>
-                         </div>
-                                                      
 
-                         
-                    </div>
-          </div>
-     </section>
+          <div class="content">
+          <header class="text-center">
+               <h2>Log In</h2>
+          </header>
+
+          <form action="<?php echo FRONT_ROOT ?>User/LoginValidation" method="post" class="login-form bg-dark-alpha p-5">
+               <div class="form-group">
+                    <label for="">Email</label>
+                    <input type="email" name="email" value="" class="form-control form-control-lg" placeholder="Email" required>
+               </div>
+               <div class="form-group">
+                    <label for="">Password</label>
+                    <input type="password" name="password" value="" class="form-control form-control-lg" placeholder="Password" required>
+               </div>
+               <button class="btn btn-dark btn-block btn-lg" type="submit">Iniciar Sesión</button>
+               <?php echo '<a class="btn btn-primary btn-block btn-lg"" href="' . htmlspecialchars($loginUrl) . '">Or log in with Facebook!</a>'; ?>
+          </form>
 </main>
--- Views/register.php
+++ Views/register.php
@@ -3,40 +3,28 @@
     require_once(VIEWS_PATH."fbload.php");
 ?>
 
-<main class="py-5">
-     <section id="listado" class="mb-5">
-          <div class="container">
-               <h2 class="mb-4">Register</h2>
-               <form action="<?php echo FRONT_ROOT ?>User/RegisterValidation" method="post" class="bg-light-alpha p-5">
-                    <div class="row">                         
-                         <div class="col-lg-4">
-                              <div class="form-group">
-                                   <input type="hidden" name="role" value="User">
-                                   <label for="">Email</label>
-                                   <input type="email" name="email" value="" class="form-control" required>
-                              </div>
-                         </div>
-                         <div class="col-lg-4">
-                              <div class="form-group">
-                                   <label for="">Password</label>
-                                   <input type="password" name="password" value="" class="form-control" required>
-                              </div>
-                         </div>
-                         <div class="col-lg-4">
-                              <div class="form-group">
-                                   <label for="">Confirm Password</label>
-                                   <input type="password" name="password2" value="" class="form-control" required>
-                              </div>
-                         </div>
-                         </div>
-                         <div>
-                              <div class="form-group">
-                              <button type="submit" name="button" class="btn btn-dark ml-auto d-block ">Register</button>
-                              <?php echo '<a class="btn btn-primary ml-auto float-right" href="' . htmlspecialchars($loginUrl) . '">Or Continue with Facebook!</a>'; ?>
-                              </div>
-                         </div>
+
+<main class="d-flex align-items-center justify-content-center height-100 py-5">
+          <div class="content">
+               <header class="text-center">
+                    <h2>Register</h2>
+               </header>
+
+               <form action="<?php echo FRONT_ROOT ?>User/RegisterValidation" method="post" class="login-form bg-dark-alpha p-5">
+                    <div class="form-group">
+                         <label for="">Email</label>
+                         <input type="email" name="email" value="" class="form-control form-control-lg" placeholder="Email" required>
                     </div>
+                    <div class="form-group">
+                         <label for="">Password</label>
+                         <input type="password" name="password" value="" class="form-control form-control-lg" placeholder="Password" required>
+                    </div>
+                    <div class="form-group">
+                         <label for="">Confirm Password</label>
+                         <input type="password" name="password2" value="" class="form-control" placeholder="Repeat Password" required>
+                    </div>
+                    <button class="btn btn-dark btn-block btn-lg" type="submit">Register</button>
+                    <?php echo '<a class="btn btn-primary btn-block btn-lg"" href="' . htmlspecialchars($loginUrl) . '">Or Continue with Facebook!</a>'; ?>
                </form>
           </div>
-     </section>
 </main>
