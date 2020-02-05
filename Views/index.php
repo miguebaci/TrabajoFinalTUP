960cdc6277cd6c521373eae8529334120ed2e894
miguebaci <miguebaci@gmail.com>
2020-02-05 18:36:02 -0300

    slight changes to de carousel visual

--- Views/index.php
+++ Views/index.php
@@ -4,7 +4,7 @@ require_once('get-active.php');
 ?>
 
 <main class="py-5">
-     <div class="slideshow-container bg-secondary py-1">
+     <div class="slideshow-container py-1">
           <form action="<?php echo FRONT_ROOT ?>Function/SelectSlider" method="post" class="bg-light-alpha p-5">
                <?php
                foreach ($movieList as $movie) {
@@ -19,21 +19,24 @@ require_once('get-active.php');
                                         </div>
                                         <div class="row">
                                              <div class="column">
-                                                  <button type="sumbit" class="btn btn-outline-light" name="idMovie_Selected" value="<?php echo $movie->getIdMovie(); ?>"><?php echo "<" . POSTER_ROOT . $movie->getImage() . " width='180' height='240'>" ?></button>
+                                                  <button type="sumbit" class="btn btn-outline-light" name="idMovie_Selected" value="<?php echo $movie->getIdMovie(); ?>"><?php echo "<" . POSTER_ROOT . $movie->getImage() . " width='360' height='480'>" ?></button>
                                              </div>
-                                             <div class="column"></div>
-                                             <div class="column" style="margin-left:50px">
-                                                  <h5><?php echo $movie->getMovieName() ?></h5>
-                                                  <br><br>
-                                                  <h5>Duration: <?php echo $movie->getDuration() ?> min </h5>
-                                                  <br><br>
-                                                  <h5>Genres: <?php $genreArray = $movie->getGenre();
-                                                                      foreach ($genreArray as $genres) {
-                                                                           echo $genres->getDescription();
-                                                                           if (next($genreArray)) {
-                                                                                echo "/";
-                                                                           }
-                                                                      } ?></h5>
+                                                  <div class="column" style="margin-left:50px">
+                                                  <div class="row py-5">
+                                                       <h3><?php echo $movie->getMovieName() ?></h3>
+                                                  </div>
+                                                  <div class="row py-5">
+                                                       <h5>Duration: <?php echo $movie->getDuration() ?> min </h5>
+                                                  </div>
+                                                  <div class="row py-5">
+                                                       <h5>Genres: <?php $genreArray = $movie->getGenre();
+                                                                           foreach ($genreArray as $genres) {
+                                                                                echo $genres->getDescription();
+                                                                                if (next($genreArray)) {
+                                                                                     echo "/";
+                                                                                }
+                                                                           } ?></h5>
+                                                  </div>
                                              </div>
                                         </div>
                                    </div>
